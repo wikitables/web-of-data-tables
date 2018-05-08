35fca106c0dcf5753c14ca9fdcf3b20efc9bca59
Emir Muñoz <emir.munoz@gmail.com>
2018-05-08 13:26:49 +0100

    Format and document the script

--- source/bin/wikipedia_crawler.py
+++ source/bin/wikipedia_crawler.py
@@ -2,21 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import json
-import time
-import requests
-import logging
-import threading
-import multiprocessing
+import bz2
 import itertools
+import logging
+import multiprocessing
+import os
 import re
 import sys
-import bz2
-from math import floor
-from xml.etree import cElementTree
+import threading
+import time
 from functools import partial
+from xml.etree import cElementTree
+
+import requests
 from smart_open import smart_open
-from bs4 import BeautifulSoup
+
+# from bs4 import BeautifulSoup
 
 
 __author__ = 'emunoz'
@@ -50,7 +51,8 @@ An example on how to use the API is:
 $ curl -X GET --header 'Accept: text/html; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/HTML/1.6.1"' \
 'https://en.wikipedia.org/api/rest_v1/page/html/Isaac%20Alb%C3%A9niz'
 
-The script takes several parts from [3,4], where the goal is to parse the Wikipedia XML dump and extract sequences of words.
+The script takes several parts from [3,4], where the goal is to parse the Wikipedia XML dump and extract 
+sequences of words.
 
 [1] https://en.wikipedia.org/api/rest_v1/
 [2] https://www.mediawiki.org/wiki/RESTBase
@@ -63,13 +65,15 @@ def parse_cli_arguments(argv):
     def formatter(prog):
         return argparse.HelpFormatter(prog, max_help_position=100, width=200)
 
-    argparser = argparse.ArgumentParser('Parser of Wikipedia Tables', formatter_class=formatter)
-    
+    argparser = argparse.ArgumentParser('Crawler of Wikipedia articles', formatter_class=formatter)
+
     nb_jobs_default = max(1, multiprocessing.cpu_count() - 1)
 
     # General options
     argparser.add_argument('--file', '-f', type=str, default=None,
                            help='Path to MediaWiki dump for read-only', required=True)
+    argparser.add_argument('--output', '-o', type=str, default='./wikipedia_html',
+                           help='Path to output directory')
     argparser.add_argument('--jobs', '-j', type=int, default=nb_jobs_default,
                            help='Number of jobs to run. Default (#cpus - 1)')
     argparser.add_argument('--debug', '-d', action='store_const', dest='loglevel',
@@ -85,25 +89,58 @@ class WikipediaSpider(object):
     Based on ratelimit [1] functions.
     
     [1] https://github.com/tomasbasham/ratelimit/blob/master/ratelimit/decorators.py
+
+    Parameters
+    ----------
+    calls : int
+        Number of call in the `period`. Default 15
+    period : int
+        Seconds of the period. Default: 60
+    clock : time.time
+        Clock time object when started.
+    raise_on_limit : bool
+        Whether to raise an error when reaching the limit or not. Default: True
+    output : str
+        Path to the output folder.
     """
-    def __init__(self, calls=15, period=60, clock=None, raise_on_limit=True):
+
+    def __init__(self, calls=15, period=60, clock=None, raise_on_limit=True, output='.'):
         self.clamped_calls = max(1, calls)
         self.period = period
         self.clock = clock
         self.raise_on_limit = raise_on_limit
+        self.output = output
         self.last_reset = clock()
         self.num_calls = 0
         # Add thread safety.
         self.lock = threading.RLock()
 
     def get_html(self, article_title):
+        """ Retrieve the HTML text from the API.
+
+        A thread is created to handle each request.
+
+        Parameters
+        ----------
+        article_title : str
+            Article title.
+        """
         thread = threading.Thread(target=self.download_article, args=(article_title,))
         thread.daemon = True
         thread.start()
-    
+
     def download_article(self, article_title):
+        """ Method to query the API and retrieve the HTML text.
+
+        The HTML is then saved to disk compressed using bz2.
+
+        Parameters
+        ----------
+        article_title : str
+            Article title.
+        """
         api_url = '{0}page/html/{1}?redirect=false'.format(api_url_base, article_title)
-        
+
         with self.lock:
             period_remaining = self.__period_remaining()
             # If the time window has elapsed then reset.
@@ -113,40 +150,38 @@ class WikipediaSpider(object):
 
             # Increase the number of attempts to call the function.
             self.num_calls += 1
-            
+
             # If the number of attempts to call the function exceeds the
-            # maximum then raise an exception.
+            # maximum then sleep the thread for half a second.
             if self.num_calls > self.clamped_calls:
                 logger.info('Sleeping for half a second')
                 time.sleep(0.5)
                 return
 
+        # query the API
         response = requests.get(api_url, headers=headers)
 
         if response.status_code == 200:
-            # print(response.headers)
-            #return response.content.decode('utf-8')
+            # return response.content.decode('utf-8')
             html_text = response.content.decode('utf-8')
-            
+
             # fix the base and css
             # soup = BeautifulSoup(html_text, 'html.parser')
             # soup.base['href'] = 'http://en.wikipedia.org/wiki/'
             # print(soup.base)
-        
+
             # links stay relative https://stackoverflow.com/a/44002598
             # https://codereview.stackexchange.com/questions/100490/extracting-and-normalizing-urls-in-an-html-document
             # http://www.compjour.org/warmups/govt-text-releases/extracting-absolute-wh-press-briefings-urls/
             # links = soup.find_all('a')
             # print(links)
 
-            with bz2.BZ2File('./wikipedia_html/{0}.html.bz2'.format(article_title), mode='w') as bz2f:
-                # bz2f.write(str.encode(str(soup)))
+            with bz2.BZ2File('{0}/{1}.html.bz2'.format(self.output, article_title), mode='w') as bz2f:
                 bz2f.write(str.encode(html_text))
-            
         else:
             print('[!] HTTP {0} calling [{1}]'.format(response.status_code, api_url))
             # return None
-            
+
     def __period_remaining(self):
         """
         Return the period remaining for the current rate limit window.
@@ -157,7 +192,90 @@ class WikipediaSpider(object):
         return self.period - elapsed
 
 
+class WikiParser(object):
+    """ Treats a wikipedia articles dump (<LANG>wiki-<YYYYMMDD>-pages-articles.xml.bz2
+    or <LANG>wiki-latest-pages-articles.xml.bz2) as a (read-only) corpus.
+
+    The documents are extracted on-the-fly, so that the whole (massive) dump can stay compressed on disk.
+
+    Parameters
+    ----------
+    wiki_dump : object
+        File object of the Wikipedia XML dump.
+    min_article_character : int
+        Minimum number of characters to consider in one article.
+    nb_jobs : int
+        Number of jobs to use.
+    """
+
+    def __init__(self, wiki_dump, min_article_character=200, nb_jobs=None):
+        self.wiki_dump = wiki_dump
+        self.nb_jobs = nb_jobs
+        self.min_article_character = min_article_character
+        self.nb_articles = 0
+
+    def get_articles(self):
+        """ Parse the XML dump and retrieve the titles and sections.
+
+        Returns
+        -------
+        Generator
+            Pair of title and sections of each article.
+        """
+        skipped_redirect = 0
+        skipped_disambiguation = 0
+        skipped_namespace = 0
+        skipped_length = 0
+        total_articles = 0
+        xml_pages = parse_xml_pages(self.wiki_dump)
+        # http://chriskiehl.com/article/parallelism-in-one-line/
+        pool = multiprocessing.Pool(self.nb_jobs)
+
+        # process the whole XML dump file in small chunks
+        for group in chunksize_groups(xml_pages, chunksize=10 * self.nb_jobs):
+            for article in pool.imap(partial(segment), group):
+                article_title, sections = article[0], article[1]
+                # filtering of articles such as redirect
+                # filter non-articles
+                if any(article_title.startswith(ignore + ':') for ignore in IGNORED_NAMESPACES):
+                    skipped_namespace += 1
+                    continue
+                # filter redirect
+                if not sections or sections[0][1].lstrip().lower().startswith("#redirect"):
+                    skipped_redirect += 1
+                    continue
+                # filter disambiguation pages
+                if article_title.endswith('(disambiguation)'):
+                    skipped_disambiguation += 1
+                    continue
+                # filter stubs (incomplete, very short articles)
+                if sum(len(body.strip()) for (_, body) in sections) < self.min_article_character:
+                    skipped_length += 1
+                    continue
+
+                total_articles += 1
+                yield (article_title, sections)
+
+        logger.info('finished processing %i articles '
+                    '(skipped %i redirects, %i disambiguation, %i stubs, %i ignored namespaces)',
+                    total_articles, skipped_redirect, skipped_disambiguation, skipped_length, skipped_namespace)
+        pool.terminate()
+        self.nb_articles = total_articles
+
+
 def get_namespace(tag):
+    """ Return the namespace of a tag.
+
+    Parameters
+    ----------
+    tag : str
+        Tag string.
+
+    Returns
+    -------
+    str
+        Identified namespace.
+    """
     m = re.match("^{(.*?)}", tag)
     namespace = m.group(1) if m else ""
     if not namespace.startswith("http://www.mediawiki.org/xml/export-"):
@@ -166,12 +284,24 @@ def get_namespace(tag):
 
 
 def parse_xml_pages(xml_dump):
+    """ Parse an XML page from the dump.
+
+    Parameters
+    ----------
+    xml_dump : object
+        File object of the Wikipedia XML dump.
+
+    Returns
+    -------
+    Generator
+        Iterator over page elements as string.
+    """
     elems = (elem for _, elem in cElementTree.iterparse(xml_dump, events=("end",)))
     elem = next(elems)
     namespace = get_namespace(elem.tag)
     ns_mapping = {"ns": namespace}
     page_tag = "{%(ns)s}page" % ns_mapping
-    
+
     for elem in elems:
         # only retrieve pages
         if elem.tag == page_tag:
@@ -180,19 +310,33 @@ def parse_xml_pages(xml_dump):
 
 
 def segment(page_xml):
+    """ Segment a given XML page.
+
+    Parameters
+    ----------
+    page_xml : str
+        XML page as string.
+
+    Returns
+    -------
+    str
+        Page title.
+    list
+        Page sections.
+    """
     elem = cElementTree.fromstring(page_xml)
     namespace = get_namespace(elem.tag)
     ns_mapping = {"ns": namespace}
     text_path = "./{%(ns)s}revision/{%(ns)s}text" % ns_mapping
     title_path = "./{%(ns)s}title" % ns_mapping
-    ns_path = "./{%(ns)s}ns" % ns_mapping
+    # ns_path = "./{%(ns)s}ns" % ns_mapping
     lead_section_heading = "Introduction"
     top_level_heading_regex = r"\n==[^=].*[^=]==\n"
     top_level_heading_regex_capture = r"\n==([^=].*[^=])==\n"
-    
+
     title = elem.find(title_path).text
     text = elem.find(text_path).text
-    ns = elem.find(ns_path).text
+    # ns = elem.find(ns_path).text
 
     # currently filtering only the first paragraph
     if text is not None:
@@ -203,13 +347,27 @@ def segment(page_xml):
     else:
         section_contents = []
         section_headings = []
-	
+
     sections = list(zip(section_headings, section_contents))
 
     return title, sections
 
 
-def chunksize_groups(iterable, chunksize, maxsize=0):
+def chunksize_groups(iterable, chunksize):
+    """ Chunk the stream of pages in groups of a given size.
+
+    Parameters
+    ----------
+    iterable : Iterable
+        Stream of XML parts.
+    chunksize : int
+        Size of the chunk to use.
+
+    Returns
+    -------
+    Generator
+        Iterator over the chunk groups.
+    """
     it = iter(iterable)
     while True:
         wrapped_chunk = [list(itertools.islice(it, int(chunksize)))]
@@ -218,85 +376,62 @@ def chunksize_groups(iterable, chunksize, maxsize=0):
         yield wrapped_chunk.pop()
 
 
-class WikiParser():
-    """ Treats a wikipedia articles dump (<LANG>wiki-<YYYYMMDD>-pages-articles.xml.bz2
-    or <LANG>wiki-latest-pages-articles.xml.bz2) as a (read-only) corpus.
-    The documents are extracted on-the-fly, so that the whole (massive) dump can stay compressed on disk.
+def parse_all_articles(xml_dump, min_article_character, nb_jobs=None):
+    """ Parse the Wikipedia XML dump and retrieve page by page.
+
+    Parameters
+    ----------
+    xml_dump : str
+        Path to the Wikipedia XML dump file.
+    min_article_character : int
+        Minimum number of characters to consider in one article.
+    nb_jobs : int
+        Number of jobs to use.
+
+    Returns
+    -------
+    Generator
+        Iterator for the filtered articles.
     """
-    def __init__(self, wiki_dump, min_article_character=200, nb_jobs=None):
-        self.wiki_dump = wiki_dump
-        self.nb_jobs = nb_jobs
-        self.min_article_character = min_article_character
-        self.nb_articles = 0
-	
-    def get_articles(self):
-        skipped_redirect = 0
-        skipped_disambiguation = 0
-        skipped_namespace = 0
-        skipped_length = 0
-        total_articles = 0
-        xml_pages = parse_xml_pages(self.wiki_dump)
-        # http://chriskiehl.com/article/parallelism-in-one-line/
-        pool = multiprocessing.Pool(self.nb_jobs)
-        
-	    # process the whole XML dump file in small chunks
-        for group in chunksize_groups(xml_pages, chunksize=10 * self.nb_jobs, maxsize=1):
-            for article in pool.imap(partial(segment), group):
-                article_title, sections = article[0], article[1]
-	            # filtering of articles such as redirect
-                # filter non-articles
-                if any(article_title.startswith(ignore + ':') for ignore in IGNORED_NAMESPACES):
-                    skipped_namespace += 1
-                    continue
-                # filter redirect
-                if not sections or sections[0][1].lstrip().lower().startswith("#redirect"):
-                    skipped_redirect += 1
-                    continue
-                if article_title.endswith('(disambiguation)'):
-                    skipped_disambiguation += 1
-                    continue
-                # filter stubs (incomplete, very short articles)
-                if sum(len(body.strip()) for (_, body) in sections) < self.min_article_character:
-                    skipped_length += 1
-                    continue
-                # print('TITLE=',article_title)
-                # print('SEC=',sections[0][1])
-                total_articles += 1
-                yield (article_title, sections)
-	
-        logger.info(
-            "finished processing %i articles with %i sections (skipped %i redirects, %i disambiguation, %i stubs, %i ignored namespaces)",
-            total_articles, total_sections, skipped_redirect, skipped_disambiguation, skipped_length, skipped_namespace)
-        pool.terminate()
-        self.nb_articles = total_articles
-
-
-def parse_all_articles(xml_dump_path, min_article_character, nb_jobs=None):
-    with smart_open(xml_dump_path, 'rb') as dump:
+    with smart_open(xml_dump, 'rb') as dump:
         parser_dump = WikiParser(dump, min_article_character, nb_jobs)
         wiki_articles_stream = parser_dump.get_articles()
         for article in wiki_articles_stream:
             yield article
 
 
-def parse_articles(xml_dump_path, min_article_character=200, nb_jobs=None):
-    article_stream = parse_all_articles(xml_dump_path, min_article_character, nb_jobs)
-    max_count = 2
-    
-    # create a spider that respects the crawling rules
+def parse_articles(xml_dump, output, min_article_character=200, nb_jobs=None):
+    """ Parse the Wikipedia dump, retrieve the articles' title and request the HTML
+    text from the Wikipedia API.
+
+    Parameters
+    ----------
+    xml_dump : str
+        Path to the Wikipedia XML dump file.
+    output : str
+        Path to the output directory.
+    min_article_character : int
+        Minimum number of characters to consider in one article.
+    nb_jobs : int, None
+        Number of jobs to use.
+    """
+    article_stream = parse_all_articles(xml_dump, min_article_character, nb_jobs)
+    # create output directory if does not exist
+    output_path = os.path.join(' ', output)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    # create a spider that respects the crawling rules:
+    # max 200 requests per second
     now = time.monotonic if hasattr(time, 'monotonic') else time.time
-    # period in seconds
-    spider = WikipediaSpider(calls=200, period=1, clock=now, raise_on_limit=True)
+    spider = WikipediaSpider(calls=200, period=1, clock=now, raise_on_limit=True, output=output_path)
+    # max_count = 2
     for idx, article in enumerate(article_stream):
         article_title = article[0]
         # print('TITLE=',article_title)
         spider.get_html(article_title)
         # print(html_text)
-        
-        #if idx > max_count:
+        # if idx > max_count:
         #    break
-        
-        if (idx + 1) % 1000 == 0:  # 100000
+        if (idx + 1) % 100000 == 0:
             logger.info("processed #%d articles (at %r now)", idx + 1, article_title)
 
 
@@ -307,10 +442,11 @@ def main(argv):
     # User parameters
     args = parse_cli_arguments(argv)
     dump_file = args.file
+    output_dir = args.output
     nb_jobs = args.jobs
     logger.info('running %s (jobs=%d)', ' '.join(sys.argv), nb_jobs)
-    
-    parse_articles(dump_file, nb_jobs=nb_jobs)
+    parse_articles(dump_file, output=output_dir, nb_jobs=nb_jobs)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
