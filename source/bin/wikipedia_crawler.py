3c7c54d61f9b8e43741100eba4bde90f87564016
Emir Muñoz <emir.munoz@gmail.com>
2018-05-09 13:15:15 +0100

    Disabling warning messages

--- source/bin/wikipedia_crawler.py
+++ source/bin/wikipedia_crawler.py
@@ -29,7 +29,7 @@ __author__ = 'emunoz'
 logging.basicConfig(format='%(asctime)s.%(msecs)03d %(levelname)s {%(module)s} [%(funcName)s] %(message)s',
                     datefmt='%Y-%m-%d,%H:%M:%S', level=logging.INFO)
 logger = logging.getLogger('wodt')
-logger.propagate = True
+logging.getLogger(requests.packages.urllib3.__package__).setLevel(logging.ERROR)
 
 """Table markup"""
 IGNORED_NAMESPACES = [
